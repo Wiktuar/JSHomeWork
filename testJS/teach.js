b72a72e7c7ac087bded4df523a8b7c231c5a3b02
Виктор Гусев <wiktuar@yandex.ru>
2023-12-16 13:36:08 +0300

    JS Homework 1

---
+++ testJS/teach.js
@@ -0,0 +1,10 @@
+// task 1
+let celsius = 16;
+let fahrenheit = (9 / 5) * celsius + 32;
+console.log('температура по Цельсию - ' + celsius);
+console.log('температура по Фаренгейту - ' + fahrenheit);
+
+//task2
+let name = "Viktor";
+let admin = name;
+console.log(admin);
