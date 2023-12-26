9fd4f21714ffc2d8110cd97b86a4e4f32d22bb14
Виктор Гусев <wiktuar@yandex.ru>
2023-12-26 22:08:57 +0300

    done homework 4

--- testJS/teach.js
+++ testJS/teach.js
@@ -1,32 +1,25 @@
 //task1
-let func = x => Math.pow(x, 3);
-console.log(func(3));
-console.log(func(2));
-
-//task2
-let number = prompt("Введите число");
-
-(isNaN(number))?console.log("введено не число"):
-    console.log(sizeOfTax(1000));
-
-function sizeOfTax(number){
-    return Math.round(number - number*0.13);
+for (let i = 0; i < 11; i++) {
+   if(i === 0) console.log(`${i} - это ноль`);
+   else if(i % 2 !== 0) console.log(`${i} - нечетное число`);
+   else if(i % 2 === 0) console.log(`${i} - четное ноль`);
 }
 
-//task3
-let userString = prompt("Введите число");
-let array = userString.split(" ");
+//task2
+let arr = [1, 2, 3, 4, 5, 6, 7];
+arr.filter(a => a < 4 || a > 5).forEach(a => console.log(a));
 
-let minimum = array.map(x => +x).reduce((x, y) => Math.max(x, y));
-console.log(minimum);
+
+// //task3
+let filledArray = Array(10).fill(0);
+filledArray = filledArray.map(f => Math.round(Math.random()*9));
+let sum = filledArray.reduce((x, y) => x + y);
+let min = filledArray.reduce((x, y) => Math.min(x, y));
+let incl = filledArray.includes(3);
 
 //task4
-let a = 15;
-let b = 25;
-
-let sum =  (a, b) => a+b;
-let diff =  (a, b) => (a > b)? a - b : b - a;
-let pow =  (a, b) => a * b;
-let div =  (a, b) => (b !== 0)? a / b: "На ноль делить нельзя" ;
-
-console.log(div(a, b));
+let k = "x";
+for (let i = 1; i < 21; i++) {
+    console.log(k);
+    k+="X";
+}
