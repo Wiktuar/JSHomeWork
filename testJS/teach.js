314fe8005ca4398b08df18c9965ecc5e5b7cccd7
Виктор Гусев <wiktuar@yandex.ru>
2023-12-27 07:48:27 +0300

    done final homework

--- testJS/teach.js
+++ testJS/teach.js
@@ -1,25 +1,104 @@
 //task1
-for (let i = 0; i < 11; i++) {
-   if(i === 0) console.log(`${i} - это ноль`);
-   else if(i % 2 !== 0) console.log(`${i} - нечетное число`);
-   else if(i % 2 === 0) console.log(`${i} - четное ноль`);
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7
+}
+
+for(let key in numbers){
+    if(numbers[key] >= 3) console.log(numbers[key]);
 }
 
 //task2
-let arr = [1, 2, 3, 4, 5, 6, 7];
-arr.filter(a => a < 4 || a > 5).forEach(a => console.log(a));
+const post = {
+    author: "John", // вывести этот текст
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+                likes: 10,
+                dislikes: 2, // вывести это число
+            },
+        },
+        {
+            userId: 5, // вывести это число
+            userName: "Jane",
+            text: "lorem ipsum 2", // вывести этот текст
+            rating: {
+                likes: 3,
+                dislikes: 1,
+            },
+        },
+    ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[0].text);
 
 
-// //task3
-let filledArray = Array(10).fill(0);
-filledArray = filledArray.map(f => Math.round(Math.random()*9));
-let sum = filledArray.reduce((x, y) => x + y);
-let min = filledArray.reduce((x, y) => Math.min(x, y));
-let incl = filledArray.includes(3);
+//task3
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach(p => console.log(p.price*0.85));
 
 //task4
-let k = "x";
-for (let i = 1; i < 21; i++) {
-    console.log(k);
-    k+="X";
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ],
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg",
+        ],
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+products.filter(p => p.photos !== undefined && p.photos.length > 0).forEach(p => console.log(p));
+products.sort((p1, p2)=> p1.price - p2.price);
+
+//task5
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+
+let daysOfWeek = {}
+
+for (let i = 0; i < en.length; i++) {
+    daysOfWeek[en[i]] = ru[i];
 }
